8896639f6cfafd463a0b5bf5dc6b5c4d7404f8db
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-04-13 02:55:13 +0100

    create api models ✅

--- api/models.py
+++ api/models.py
@@ -1,3 +1,48 @@
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .utils import generate_id, generate_rid
+from .managers import UserManager
 
-# Create your models here.
+
+class BaseModel(models.Model):
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractUser):
+
+    uid = models.CharField(default=generate_id, max_length=32, unique=True)
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+
+class FilePDF(models.Model):
+    
+    pdf = models.FileField(upload_to="receipts")
+
+
+class Receipt(BaseModel):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rid = models.CharField(max_length=11, default=generate_rid, unique=True)
+    payload = models.JSONField()
+    files = models.ManyToManyField(FilePDF)
+
+
+
+
+
+
